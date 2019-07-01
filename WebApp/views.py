554766cdba7844da66069f188f71abd8761f44f5
hmc-hhuang-2020 <huhuang@cs.hmc.edu>
2019-07-01 16:14:50 -0700

    working on upload image to goolge cloud bucket

---
+++ WebApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy 
+
+from .forms import PostForm
+from .models import Post
+
+
+class HomePageView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'home.html'
+    success_url = reverse_lazy('home')
